81c0058d910cf405a51e4d8c1bf906dad6ed42fa
Tim Spence <timothywspence@gmail.com>
2022-08-10 15:52:55 +0100

    Reverse headers to preserve message header order

--- ember-core/shared/src/main/scala/org/http4s/ember/core/Parser.scala
+++ ember-core/shared/src/main/scala/org/http4s/ember/core/Parser.scala
@@ -176,7 +176,7 @@ private[ember] object Parser {
           start,
         ).asLeft.pure[F]
       } else {
-        HeaderP(Headers(headers.toList), chunked, contentLength, idx).asRight.pure[F]
+        HeaderP(Headers(headers.reverse), chunked, contentLength, idx).asRight.pure[F]
       }
     }
 
